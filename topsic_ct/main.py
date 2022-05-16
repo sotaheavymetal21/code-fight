c04a030c4c18a09ac27e7e65b8ca9568e80f8f95
sotaheavymetal21 <sota_anegawa@100pj.biz>
2022-05-16 09:30:59 +0900

    finished

--- topsic_ct/main.py
+++ topsic_ct/main.py
@@ -5,7 +5,7 @@ x_coordinate = 0
 y_coordinate = 0
 
 for i in range(1, N+1):
-    if i % 4 ==0:
+    if i % 4 == 0:
         y_coordinate = y_coordinate + S[i-1]
     elif i % 4 == 3:
         x_coordinate = x_coordinate - S[i-1]
@@ -15,3 +15,23 @@ for i in range(1, N+1):
         x_coordinate = x_coordinate + S[i-1]
 
 print(x_coordinate, y_coordinate)
+
+N = int(input())
+D = [int(input()) for i in range(N)]
+print(D)
+x = 0
+y = 0
+for j in range(N):
+    x = sum(D[::4]) - sum(D[2::4])
+    y = sum(D[3::4]) - sum(D[1::4])
+print(D[::4])
+print(D[2::4])
+print(D[3::4])
+print(D[1::4])
+print(x, y)
+# [3, 1, 4, 1, 5, 9]
+# [3, 5]
+# [4]
+# [1]
+# [1, 9]
+# 4 -9
